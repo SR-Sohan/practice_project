c46c230fa401181af5c0a9236cb73dd84efc938d
SR-Sohan <sr.sohanur22@gmail.com>
2023-07-24 18:42:59 +0600

    logout form

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -32,7 +32,17 @@
                         <li><a class="dropdown-item" href="#!">Settings</a></li>
                         <li><a class="dropdown-item" href="#!">Activity Log</a></li>
                         <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="#!">Logout</a></li>
+                        <li>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+            
+                                <x-responsive-nav-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </x-responsive-nav-link>
+                            </form>
+                        </li>
                     </ul>
                 </li>
             </ul>
